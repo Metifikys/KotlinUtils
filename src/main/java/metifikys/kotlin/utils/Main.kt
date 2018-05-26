9046952cb902584747945a59dd2cc4b1db681e2b
Metifikys <metifikys@gmail.com>
2018-05-26 13:59:26 +0300

    firs commit

---
+++ src/main/java/metifikys/kotlin/utils/Main.kt
@@ -0,0 +1,10 @@
+package metifikys.kotlin.utils
+
+import metifikys.kotlin.utils.extends.isEmpty
+import metifikys.kotlin.utils.extends.notEmpty
+import java.io.File
+
+fun main(args: Array<String>) {
+
+    println(File("D:\\MLP.jpg").notEmpty())
+}
---
+++ src/main/java/metifikys/kotlin/utils/extends/Files.kt
@@ -0,0 +1,6 @@
+package metifikys.kotlin.utils.extends
+
+import java.io.File
+
+fun File.isEmpty() = this.length() == 0L
+fun File.notEmpty() = this.length() > 0L
