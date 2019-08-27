0a5eedf55e8b8129c017a5aa2a0e58433c89e641
Metifikys <metifikys@gmail.com>
2019-08-27 22:08:41 +0300

    add label and ch methods

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # KotlinUtils
 
 ![travis-ci](https://travis-ci.org/Metifikys/KotlinUtils.svg?branch=master)
+[![codecov](https://codecov.io/gh/Metifikys/KotlinUtils/branch/master/graph/badge.svg)](https://codecov.io/gh/Metifikys/KotlinUtils)
--- src/main/java/metifikys/kotlin/utils/extends/Files.kt
+++ src/main/java/metifikys/kotlin/utils/extends/Files.kt
@@ -2,8 +2,12 @@ package metifikys.kotlin.utils.extends
 
 import java.io.File
 
-fun File.isEmpty() = this.length() == 0L
-fun File.notEmpty() = this.length() > 0L
+fun File.isEmpty(): Boolean {
+    return this.length() == 0L
+}
+fun File.notEmpty(): Boolean {
+    return this.length() > 0L
+}
 
 fun String.getFileFromResources() : File {
     return File(this::javaClass.javaClass.classLoader.getResource(this).path)
