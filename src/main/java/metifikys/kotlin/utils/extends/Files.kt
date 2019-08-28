b06b65a39f1c7dc152373127091314c320eabfd0
Metifikys <metifikys@gmail.com>
2019-08-28 21:52:54 +0300

    add jacoco

--- src/main/java/metifikys/kotlin/utils/extends/Files.kt
+++ src/main/java/metifikys/kotlin/utils/extends/Files.kt
@@ -2,12 +2,9 @@ package metifikys.kotlin.utils.extends
 
 import java.io.File
 
-fun File.isEmpty(): Boolean {
-    return this.length() == 0L
-}
-fun File.notEmpty(): Boolean {
-    return this.length() > 0L
-}
+fun File.isEmpty() = this.length() == 0L
+
+fun File.notEmpty() = this.length() > 0L
 
 fun String.getFileFromResources() : File {
     return File(this::javaClass.javaClass.classLoader.getResource(this).path)
